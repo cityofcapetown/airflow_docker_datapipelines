76905aeb4a2794630f21439db7055cf793ea84a4
Gordon Inggs <gordon.inggs@gmail.com>
2019-08-19 16:37:24 +0200

    Fixing typo in deploy script

--- k8s/deploy-airflow.sh
+++ k8s/deploy-airflow.sh
@@ -9,7 +9,7 @@ echo "> Creating airflow Namespaces" &&
   echo "> Creating 'dags' and 'logs' persistent volume claims" &&
   kubectl apply -f resources/airflow_volume_claims.yaml &&
   echo "> Generating a strong fernet key" &&
-  sed -in 's/.*fernetKey: ".*/  fernetKey: "'"$(openssl rand -base64 32 | tr -d "\n")"'"/' resources/airflow-values.yaml &&
+  sed -in 's/.*fernetKey: "".*/  fernetKey: "'"$(openssl rand -base64 32 | tr -d "\n")"'"/' resources/airflow-values.yaml &&
   echo "> Deploying Airflow via Helm" &&
   helm upgrade --install --values resources/airflow-values.yaml --namespace airflow airflow stable/airflow &&
   echo "> I'm all done, exiting..."
